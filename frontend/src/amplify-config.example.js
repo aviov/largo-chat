f729e3987da08f032a2046fbb5cc1cb1fbe73a60
Juri L <juril@Juris-MBP.lan>
2025-03-20 09:02:54 +0200

    Remove amplify-config.js from version control

--- frontend/src/amplify-config.example.js
+++ frontend/src/amplify-config.example.js
@@ -1,14 +1,14 @@
 const awsconfig = {
     Auth: {
       region: 'eu-central-1', // Update to your region
-      userPoolId: 'eu-central-1_vuBhAGI2h', // From CDK output
-      userPoolWebClientId: '2v3eiooib760odbkcgg949op8j', // From CDK output
+      userPoolId: 'USER_POOL_ID', // From CDK output
+      userPoolWebClientId: 'USER_POOL_CLIENT_ID', // From CDK output
     },
     API: {
       endpoints: [
         {
           name: 'ChatApi',
-          endpoint: 'https://z159wnpazl.execute-api.eu-central-1.amazonaws.com/prod/', // From CDK output
+          endpoint: 'API_ENDPOINT', // From CDK output
           custom_header: async () => ({
             Authorization: `Bearer ${(await Auth.currentSession()).getIdToken().getJwtToken()}`,
           }),
@@ -17,7 +17,7 @@ const awsconfig = {
     },
     Storage: {
       AWSS3: {
-        bucket: 'chatbotstack-contentbucket52d4b12c-3nzaezgr3s7l', // From CDK output or AWS Console
+        bucket: 'BUCKET_NAME', // From CDK output or AWS Console
         region: 'eu-central-1', // Update to your region
       },
     },
